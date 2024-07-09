b6079369204f578fcb22f3d963f44b7c6d418831
sonia1308 <klodatanasia@gmail.com>
2024-07-09 17:43:20 +0400

    first changes made

--- src/page/TimeLine/index.jsx
+++ src/page/TimeLine/index.jsx
@@ -8,25 +8,3 @@
      </div>
    )
  }
-
-// import { VList } from "virtua";
-
-// export default function Timeline() {
-//   return (
-//     <VList style={{ height: 800 }}>
-//     {Array.from({ length: 1000 }).map((_, i) => (
-//       <div
-//         key={i}
-//         style={{
-//           height: Math.floor(Math.random() * 10) * 10 + 10,
-//           borderBottom: "solid 1px gray",
-//           background: "white",
-//         }}
-//       >
-//         {i}
-//       </div>
-//     ))}
-//   </VList>
-//   )
-// }
-
